b5a2019a67020544dab2c55dca4ff384641bddb6
maudmain <117314424+maudmain@users.noreply.github.com>
2023-03-05 12:18:30 +0000

    init and manager prompt fucntion added

--- index.js
+++ index.js
@@ -13,3 +13,79 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+//empty array for the team members
+const teamMembers = [];
+
+
+
+const init = () => {
+    console.info(
+        "\n",
+        "-------------------- Welcome to your Team Directory --------------------",
+        "\n",
+        "Answer the questions below to generate your team",
+        "\n"
+    )
+    managerPrompt()
+
+}
+
+const managerPrompt = () => {
+    console.info('Please enter the following information for your Team Manager.')
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the team manager\'s name?'
+        }
+    ])
+        .then((answers) => {
+            const manager = new Manager(
+                answers.name
+            );
+            // adds the manager to the team members array
+            teamMembers.push(manager);
+            console.log(teamMembers);
+            console.info('Manager added to the team!')
+            newEmployee();
+        });
+};
+
+const newEmployee = () => {
+    console.info('What would you like to do next?');
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'nextChoice',
+                message: 'What would you like to do?',
+                choices: ['Add an Engineer', 'Add an Intern', 'Finish building the team']
+            }
+        ])
+        .then((answer) => {
+            if (answer.nextChoice === 'Add an Engineer') {
+                inquirer.prompt([
+                    {
+                        type: 'input',
+                        name: 'name',
+                        message: 'What is the engineer\'s name?'
+                    }
+                ])
+                    .then((answers) => {
+                        const engineer = new Engineer(
+                            answers.name
+                        );
+                        // add the engineer to the team member array
+                        teamMembers.push(engineer);
+                        console.log(teamMembers);
+                        console.info('Engineer added to the team!')
+                })
+            }
+            else if (answer.nextChoice === 'Add Intern') {
+                inquirer.prompt
+            }
+        }
+        )
+};
+
+init();
